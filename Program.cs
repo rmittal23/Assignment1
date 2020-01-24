b82a192dc7cc31a83a0b27df5a842c4f865d7c21
RISHABH MITTAL <ris.23.mit@gmail.com>
2020-01-24 18:46:30 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive integer number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            while (n > 0)
+            {
+                for (int i = n; i > 0; i--)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+                n--;
+            }
+           
+                Console.Write("Enter a positive integer number: ");
+                int k = Convert.ToInt32(Console.ReadLine());
+                int j = 0;   
+                for(int i=1; i<=k; i++)
+                    {
+                     j = j + i;   
+                     Console.Write(j + ",");
+                     
+                    }
+        }
+            
+    }
+}
